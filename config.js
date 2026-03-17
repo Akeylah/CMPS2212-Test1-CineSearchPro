d5c9c02f2100e8bc99d2f05cdcea595cc2d5fd16
Akeylah <112662745+Akeylah@users.noreply.github.com>
2026-03-17 01:23:37 +0000

    Html&Config files: Completed

---
+++ config.js
@@ -0,0 +1,11 @@
+// ==========================================
+// TMDB API CONFIGURATION
+// ==========================================
+
+const TMDB_API_KEY = "REMOVED";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const SEARCH_URL = `${BASE_URL}/search/movie`;
+
+const MOVIE_DETAILS_URL = `${BASE_URL}/movie`;
