1f86090fd17d17810437b155062a468e05ccdb73
Akeylah <112662745+Akeylah@users.noreply.github.com>
2026-03-18 15:38:39 +0000

    Update

--- Connection.js
+++ Connection.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const TMDB_API_KEY = "REMOVED";
+=======
+const API_KEY = process.env.API_KEY;
+>>>>>>> 4886e57 (Update)
 const BASE_URL = "https://api.themoviedb.org/3";
 const SEARCH_URL = `${BASE_URL}/search/movie`;
 const MOVIE_DETAILS_URL = `${BASE_URL}/movie`;
