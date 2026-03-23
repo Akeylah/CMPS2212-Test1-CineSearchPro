6a35e0e5a43373210644bc1ad040ffad71bf70c8
Akeylah <112662745+Akeylah@users.noreply.github.com>
2026-03-23 03:20:44 +0000

    temp before cleanup

---
+++ connection.js
@@ -0,0 +1,4 @@
+const TMB_API_KEY = process.env.API_KEY;;
+const BASE_URL = "https://api.themoviedb.org/3";
+const SEARCH_URL = `${BASE_URL}/search/movie`;
+const MOVIE_DETAILS_URL = `${BASE_URL}/movie`;
